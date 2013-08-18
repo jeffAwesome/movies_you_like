b5780efec3e33f9996e9574b12793d507158c1eb
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-08-17 23:26:22 -0500

    Update Playlist show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,7 +34,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.html { redirect_to playlists_path(),  notice: 'Movie was successfully created.' }
         format.json { render action: 'show', status: :created, location: @movie }
       else
         format.html { render action: 'new' }

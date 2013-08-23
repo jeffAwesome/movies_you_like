cd47c6e406b79d6a84f4252d7359681e5172b733
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-08-23 00:00:18 -0500

    Assets try

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.assets.initalize_on_precompile = false
+
 module MoviesYouLike
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

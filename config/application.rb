f052dd5bb651963ab26cab259094708667f7f2cd
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-08-23 00:02:44 -0500

    Fix bug for config assets

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.initalize_on_precompile = false
 
 module MoviesYouLike
   class Application < Rails::Application
@@ -14,6 +13,7 @@ module MoviesYouLike
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.initalize_on_precompile = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'

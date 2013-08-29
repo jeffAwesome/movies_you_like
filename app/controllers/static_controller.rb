888f264dcf789783b158ca00ae77bfa8205b4f4b
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-08-28 23:31:28 -0500

    Fix production bugs
    * Fix bug with playlists and views

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,6 @@
 class StaticController < ApplicationController
   def index
-    if user_signed_in? && current_user.playlists.count == 0
-      Playlist.create(title: 'default', user_id: current_user.id)
-    end
-
+    signed_in_playlists
     if cookies[:mov_id] && user_signed_in?
       redirect_to "/static/view?movieID=#{cookies[:mov_id]}"
       cookies.delete :mov_id
@@ -16,17 +13,25 @@ class StaticController < ApplicationController
   end
 
   def view
+    signed_in_playlists
     cookies[:mov_id] = params[:movieID] unless user_signed_in?
-
+    if user_signed_in?
+      @playlists = current_user.playlists.all
+    end
     @playlist = Playlist.new
     @movie = Movie.new
-
   end
 
   def exactMovie
   end
 
   def about
+  end
+
+  def signed_in_playlists
+    if user_signed_in? && current_user.playlists.count == 0
+      Playlist.create(title: 'default', user_id: current_user.id)
+    end
   end
 
 end

a3fa6c42a3ff8085a79e5112d78554790c9dcfc0
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-08-29 23:14:24 -0500

    Add playlist description to db

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -85,6 +85,6 @@ class PlaylistsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def playlist_params
-      params.require(:playlist).permit(:title, :user_id)
+      params.require(:playlist).permit(:title, :user_id, :about_playlist)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130821051318) do
+ActiveRecord::Schema.define(version: 20130830020835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20130821051318) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.text     "about_playlist"
   end
 
   add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
